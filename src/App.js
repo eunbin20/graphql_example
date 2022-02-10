fa2427731f22475d943efb5a63595afc72371ae0
eunbin <eunbines2@gmail.com>
2022-02-10 18:51:31 +0900

    HOC를 이용한 쿼리 예제 구현

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { gql } from 'apollo-boost';
+import { Query } from 'react-apollo';
+
+const GET_NOTICE = gql`
+  query {
+		listNotice(data: { page: 1, pageSize: 100, filter: null }) {
+			list {
+				createdAt
+				noticeId
+				title
+				contents
+			}
+		}
+	}
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>GraphQL + Apollo Client로 데이터 요청해보기</h1>
+      <Query query={GET_NOTICE}>
+        {({ loading, error, data }) => {
+          if (loading) return <p>Loading...</p>;
+          if (error) return <p>Error!(</p>;
+    
+          return (
+            <ul>
+              {data.listNotice.list.map((notice) => {
+                return (
+                  <React.Fragment key={notice.noticedId}>
+                    <h2>{notice.title}</h2>
+                    <p>{notice.contents}</p>
+                    <p>발행 날짜: {notice.createdAt}</p>
+                  </React.Fragment>
+                )
+              })}
+            </ul>
+          );
+        }}
+      </Query>
+    </>
   );
 }
 
